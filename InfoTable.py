cfb70d9915c715492cadbab043a68d2587e43fdd
Игорь Алиев <aliev.igor1@mail.ru>
2023-12-05 23:43:00 +0300

    The task is done

--- InfoTable.py
+++ InfoTable.py
@@ -1,7 +1,7 @@
-V = []
+V = {}
 def setV(a1, a2, a3, b1, b2, b3, c1, c2, c3, v):
   num = ((((((((a1*3)+a2)*3+a3)*3+b1)*3+b2)*3+b3)*3+c1)*3+c2)*3+c3
-  V.insert(num, v)
+  V[num] = v
 
 for i1 in range(3):
   for i2 in range(3):
@@ -12,7 +12,7 @@ for i1 in range(3):
             for i7 in range(3):
               for i8 in range(3):
                 for i9 in range(3):
-                  setV(i1,i2,i3,i4,i5,i6,i7,i8,i9,0.5);
+                  setV(i1,i2,i3,i4,i5,i6,i7,i8,i9,0.5)
                   if i1==i2 and i2==i3 and i3==1: setV(i1,i2,i3,i4,i5,i6,i7,i8,i9,1)
                   if i4==i5 and i5==i6 and i6==1: setV(i1,i2,i3,i4,i5,i6,i7,i8,i9,1)
                   if i7==i8 and i8==i9 and i9==1: setV(i1,i2,i3,i4,i5,i6,i7,i8,i9,1)
@@ -30,7 +30,7 @@ for i1 in range(3):
                   if i2==i5 and i5==i8 and i8==2: setV(i1,i2,i3,i4,i5,i6,i7,i8,i9,0)
                   if i3==i6 and i6==i9 and i9==2: setV(i1,i2,i3,i4,i5,i6,i7,i8,i9,0)
 
-  with open(r'C:\Users\aliev\Downloads\test.txt', 'w') as fp:
-    for item in V:
-      fp.write("%s\n" % item)
-    print('Done')
+  with open(r'test.txt', 'w') as fp:
+    for key, value in V.items():
+      fp.write(f'{int(key)},{float(value)}\n')
+    fp.close()
--- main.py
+++ main.py
@@ -1,28 +1,59 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
+
+
 class RandomAgent:
-    def choose_action(self, state, possible_actions):
+    def choose_action_from_txt(self, state, possible_actions):
         return random.choice(possible_actions)
+
+
 class QLearningAgent:
     def __init__(self, epsilon=0.1, alpha=0.2, gamma=0.9):
         self.epsilon = epsilon  # параметр исследования
-        self.alpha = alpha      # скорость обучения
-        self.gamma = gamma      # дисконт-фактор
-        self.q_values = {}      # Q-значения
+        self.alpha = alpha  # скорость обучения
+        self.gamma = gamma  # дисконт-фактор
+        #self.q_values = {}  # Q-значения
+        self.Q = {}
+        self.x = []
+        self.y = []
+    def write_q(self):
+        file = open('info.txt', 'w')
+        for key, value in self.Q.items():
+            file.write(f'{key}, {value}\n')
+        file.close()
+    def get_q(self):
+        file = open('test.txt', 'r')
+        for line in file:
+            key = int(line.rstrip('\n').split(',')[0])
+            value = float(line.rstrip('\n').split(',')[1])
+            self.Q[key] = float(value)
+        file.close()
+    def getQ(self, a1, a2, a3, b1, b2, b3, c1, c2, c3):
+        Id = ((((((((a1 * 3) + a2) * 3 + a3) * 3 + b1) * 3 + b2) * 3 + b3) * 3 + c1) * 3 + c2) * 3 + c3
+        return Id
+    """def get_q_value(self, state, action):
+        return self.q_values.get((state, action), 0.0)"""
 
-    def get_q_value(self, state, action):
-        return self.q_values.get((state, action), 0.0)
-
-    def choose_action(self, state, possible_actions):
+    """def choose_action(self, state, possible_actions):
         if random.uniform(0, 1) < self.epsilon:
             return random.choice(possible_actions)  # случайное действие с вероятностью epsilon
         else:
             q_values = [self.get_q_value(state, a) for a in possible_actions]
             max_q = max(q_values)
             best_actions = [a for a, q in zip(possible_actions, q_values) if q == max_q]
+            return random.choice(best_actions)"""
+
+    def choose_action_from_txt(self, state, possible_actions):
+        if random.uniform(0, 1) < self.epsilon:
+            return random.choice(possible_actions)  # случайное действие с вероятностью epsilon
+        else:
+            q_values = [self.Q.get(self.getQ(state[0][0], state[0][1], state[0][2], state[1][0], state[1][1], state[1][2], state[2][0], state[2][1], state[2][2])) for a in possible_actions]
+            max_q = max(q_values)
+            best_actions = [a for a, q in zip(possible_actions, q_values) if q == max_q]
             return random.choice(best_actions)
 
-    def update_q_value(self, state, action, reward, next_state):
+    """def update_q_value(self, state, action, reward, next_state):
         possible_actions_next = self.get_possible_actions(next_state)
         if not possible_actions_next:
             max_q_next = 0  # если действий нет, считаем максимальное Q-значение равным 0
@@ -31,16 +62,29 @@ class QLearningAgent:
 
         new_q = (1 - self.alpha) * self.get_q_value(state, action) + \
                 self.alpha * (reward + self.gamma * max_q_next)
-        self.q_values[(state, action)] = new_q
+        self.q_values[(state, action)] = new_q"""
+
+    def update_q_value_from_txt(self, state, action, reward, next_state):
+        possible_actions_next = self.get_possible_actions(next_state)
+        if not possible_actions_next:
+            max_q_next = 0  # если действий нет, считаем максимальное Q-значение равным 0
+        else:
+            max_q_next = max([self.Q.get(self.getQ(state[0][0], state[0][1], state[0][2], state[1][0], state[1][1], state[1][2], state[2][0], state[2][1], state[2][2])) for a in possible_actions_next])
+
+        new_q = (1 - self.alpha) * self.Q.get(self.getQ(state[0][0], state[0][1], state[0][2], state[1][0], state[1][1], state[1][2], state[2][0], state[2][1], state[2][2])) + \
+                self.alpha * (reward + self.gamma * max_q_next)
+        self.Q[self.Q.get(self.getQ(state[0][0], state[0][1], state[0][2], state[1][0], state[1][1], state[1][2], state[2][0], state[2][1], state[2][2]))] = new_q
 
     def get_possible_actions(self, state):
         return [i for i, val in enumerate(state) if val == 0]
+
 
 def print_board(board):
     for row in board:
         row_str = " | ".join(map(str, row))
         print(row_str)
         print("-" * len(row_str))
+
 
 def check_winner(board):
     for row in board:
@@ -60,18 +104,20 @@ def check_winner(board):
     return 0
 
 
-def train_agents(smart_agent, random_agent, num_episodes=100000):
+def train_agents(smart_agent, random_agent, num_episodes=200000):
     wins = 0
     loses = 0
+    smart_agent.get_q()
+
     for match in range(num_episodes):
         board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-        current_agent = smart_agent #random.choice([smart_agent, random_agent])
+        current_agent = smart_agent
 
         while True:
             state = tuple(map(tuple, board))
             possible_actions = [(i, j) for i in range(3) for j in range(3) if board[i][j] == 0]
 
-            action = current_agent.choose_action(state, possible_actions)
+            action = current_agent.choose_action_from_txt(state, possible_actions)
             row, col = action
             board[row][col] = 1  # Ход текущего агента
 
@@ -88,39 +134,40 @@ def train_agents(smart_agent, random_agent, num_episodes=100000):
                     loses += 1
 
             if current_agent == smart_agent:
-                current_agent.update_q_value(state, action, reward, next_state)
+                current_agent.update_q_value_from_txt(state, action, reward, next_state)
 
             if winner:
                 board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]  # Начать новую игру
                 break
 
             if current_agent == smart_agent:
-                current_agent = random_agent #random.choice([smart_agent, random_agent])
+                current_agent = random_agent
 
             else:
                 if current_agent == random_agent:
                     current_agent = smart_agent
-        if match % 50 == 0:
-             #print("Прошло партий")
-             #print(match)
-             #print("Победил умом")
-             print(wins)
-   # print("Сходил в молоко")
-   # print(loses)
+
+        if match % 10000 == 0:
+            smart_agent.x.append(match)
+            smart_agent.y.append(wins)
+            wins = 0
+
+
 
 def main():
     smart_agent = QLearningAgent()
     random_agent = RandomAgent()
-
     # Обучение умного агента с глупым
-    train_agents(smart_agent, random_agent, num_episodes=5000)
+    train_agents(smart_agent, random_agent, num_episodes=200000)
+    plt.plot(smart_agent.x, smart_agent.y)
+    plt.show()
 
     # Игра с обученным ботом
     board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-    while True:
+    """while True:
         state = tuple(map(tuple, board))
         possible_actions = [(i, j) for i in range(3) for j in range(3) if board[i][j] == 0]
-        action = smart_agent.choose_action(state, possible_actions)
+        action = smart_agent.choose_action_from_txt(state, possible_actions)
         row, col = action
         board[row][col] = 1  # Ход бота
 
@@ -142,8 +189,7 @@ def main():
         if winner:
             print_board(board)
             print("Игрок", "Победил!" if winner == -1 else "Проиграл!")
-            break
-
+            break"""
 
 
 if __name__ == "__main__":
